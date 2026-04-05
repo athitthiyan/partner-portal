dbfca1285ba6f6a1d6e2962d1c4f13b8fc2a5b31
athitthiyan <athitthiyananofficial@gmail.com>
2026-04-05 20:11:05 +0530

    updated test case

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/?(*.)+(spec).ts'],
+  testPathIgnorePatterns: ['<rootDir>/tests/', '<rootDir>/dist/', '<rootDir>/coverage/'],
   collectCoverageFrom: ['src/**/*.ts', '!src/main.ts'],
 };
