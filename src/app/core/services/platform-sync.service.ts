4aa2ec18ac568524df4479c14eda10fd543f9b0c
athitthiyan <athitthiyananofficial@gmail.com>
2026-04-10 20:29:42 +0530

    updated partner portaal

--- src/app/core/services/platform-sync.service.ts
+++ src/app/core/services/platform-sync.service.ts
@@ -73,10 +73,10 @@ export class PlatformSyncService implements OnDestroy {
     }
     const wsUrl = environment.apiUrl
       .replace('https://', 'wss://')
-      .replace('http://', 'ws://') + '/ws/events';
+      .replace('http://', 'ws://') + '/ws/events?token=' + token;
 
     try {
-      this.ws = new WebSocket(wsUrl, ['access_token', token]);
+      this.ws = new WebSocket(wsUrl);
       this.ws.onopen = () => {
         this.connected.set(true);
         this.reconnectAttempts = 0;
